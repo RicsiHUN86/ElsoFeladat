39a5650274e39a0b78185d44b99a5a4228288157
BRicsi <barnusrichi@gmail.com>
2024-10-18 10:21:43 +0200

    The program is loading...

--- Program.cs
+++ Program.cs
@@ -10,6 +10,33 @@ namespace ElsoFeladat
     {
         static void Main(string[] args)
         {
+            Console.Write("Add meg pontszámot (0 és 100 között): ");
+            int pontszam = int.Parse(Console.ReadLine());
+
+            if (pontszam < 0 || pontszam > 100)
+            {
+                Console.WriteLine("Érvénytelen pontszám");
+            }
+            else if (pontszam <= 49)
+            {
+                Console.WriteLine("Osztályzat: Elégtelen");
+            }
+            else if (pontszam <= 59)
+            {
+                Console.WriteLine("Osztályzat: Elégséges");
+            }
+            else if (pontszam <= 79)
+            {
+                Console.WriteLine("Osztályzat: Közepes");
+            }
+            else if (pontszam <= 89)
+            {
+                Console.WriteLine("Osztályzat: Jó");
+            }
+            else
+            {
+                Console.WriteLine("Osztályzat: Jeles");
+            }
         }
     }
 }
